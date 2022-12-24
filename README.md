520cb495b904bce68e1fdf86ba85db2c4ecf769d
developomp <developomp@gmail.com>
2022-12-24 15:24:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exyle.io Scripts
+
+- submit issues [here](https://github.com/exyleio/exyleio/issues)
+
+## [Documentation](https://exyleio-docs.web.app/docs/projects/scripts/overview)
---
+++ master-server/tool.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+print_help() {
+
+    echo "Exyle.io Deployment Helper Utility"
+    echo "GitHub: https://github.com/exyleio/exyleio-scripts"
+    echo "- Assumes it's running on Arch Linux"
+    echo
+    echo "Commands:"
+    echo "  setup"
+    echo "    - Installs required programs and shit"
+    echo "  restart"
+    echo "    - Updates"
+    echo "  backup"
+
+}
+
+setup() {
+
+    pacman -S docker zip lazydocker
+
+}
+
+restart() {
+
+    docker compose stop
+    docker compose rm -f
+    docker compose pull
+    docker compose convert >docker-compose.prod.yml
+    docker compose -f docker-compose.prod.yml up -d --remove-orphans
+
+}
+
+backup() {
+
+    # copy API log
+    # pocketbase data
+    # redis DB
+    # zip
+    # download
+    echo not implemented
+
+}
+
+case $1 in
+setup)
+    setup
+    ;;
+restart)
+    restart
+    ;;
+backup)
+    backup
+    ;;
+*)
+    print_help
+    ;;
+esac
